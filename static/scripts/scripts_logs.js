4d414d2e37dbe05606baea28eeb45d5e1c9f8e20
fbrstilo <fbrstilo@gmail.com>
2024-07-16 15:30:09 +0200

    Better regex

--- static/scripts/scripts_logs.js
+++ static/scripts/scripts_logs.js
@@ -2,14 +2,14 @@ function highlightWordsAndTimestamps() {
     const pElement = document.getElementById('text');
     let htmlContent = pElement.innerHTML;
 
-    // Highlight timestamps of format YYYY-MM-DD hh:mm:ss
+    // Highlight timestamps of format YYYY-MM-DD hh:mm:ss.ms (milliseconds optional)
     const timestampRegex = /\b\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}(\.\d+)?\b/g;
     htmlContent = htmlContent.replace(timestampRegex, match => {
         return `<span class="highlight-green">${match}</span>`;
     });
 
-    // Highlight words ending with ':'
-    const wordsWithColonRegex = /[A-Za-z]+: /gi;
+    // Highlight words ending with ':' and surrounded by spaces
+    const wordsWithColonRegex = / [^\d\s]+: /gi;
     htmlContent = htmlContent.replace(wordsWithColonRegex, match => {
         return `<span class="highlight-yellow">${match}</span>`;
     });
